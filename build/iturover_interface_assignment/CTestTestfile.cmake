8e19bee336b5939ce9b77c8abb7e6405c183bf71
omerdr1 <omerdr1@yandex.com>
2023-01-02 01:07:06 +0300

    inital commit

---
+++ build/iturover_interface_assignment/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/omer/rover_ws/src/iturover_interface_assignment
+# Build directory: /home/omer/rover_ws/build/iturover_interface_assignment
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
